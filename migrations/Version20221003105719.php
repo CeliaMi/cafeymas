de2d3a1fda4d89cb4a382a07e56cc593950cf082
Alicia Gárgoles <aliciagargoles@gmail.com>
2022-10-03 18:36:21 +0200

    cambiosAli

---
+++ migrations/Version20221003105719.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221003105719 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE cafeteria (id INT AUTO_INCREMENT NOT NULL, parque_id INT DEFAULT NULL, nombre VARCHAR(255) NOT NULL, descripcion VARCHAR(500) NOT NULL, direccion VARCHAR(255) NOT NULL, precio_cafe_con_leche DOUBLE PRECISION NOT NULL, INDEX IDX_F6AC92FC15A81C94 (parque_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE parque (id INT AUTO_INCREMENT NOT NULL, nombre VARCHAR(255) NOT NULL, descripcion VARCHAR(500) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE cafeteria ADD CONSTRAINT FK_F6AC92FC15A81C94 FOREIGN KEY (parque_id) REFERENCES parque (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE cafeteria DROP FOREIGN KEY FK_F6AC92FC15A81C94');
+        $this->addSql('DROP TABLE cafeteria');
+        $this->addSql('DROP TABLE parque');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
